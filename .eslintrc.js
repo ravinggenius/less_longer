b4cedc809c929eb165ff36219a8cd9ce5fb14945
Thomas Ingram <code@ravinggenius.com>
2018-02-04 15:18:45 -0500

    Setup linting

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true
+	},
+
+	extends: [
+		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:promise/recommended'
+	],
+
+	parserOptions: {
+		'ecmaVersion': 8
+	},
+
+	plugins: [
+		'promise'
+	],
+
+	rules: {
+		'indent': [ 'error','tab' ],
+
+		'linebreak-style': [ 'error','unix' ],
+
+		'import/order': [ 'error', {
+			'groups': [
+				[ 'builtin', 'external' ],
+				'internal',
+				'parent',
+				'sibling',
+				'index'
+			],
+			'newlines-between': 'always-and-inside-groups'
+		} ],
+
+		'quotes': [ 'error','single' ],
+
+		'semi': [ 'error','always' ]
+	}
+};
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const app = require('./source');
 
-app.listen(3000, () => console.log('Less Longer is running on port 3000'));
+app.listen(3000, () => {
+	// eslint-disable-next-line no-console
+	console.log('Less Longer is running on port 3000');
+});
