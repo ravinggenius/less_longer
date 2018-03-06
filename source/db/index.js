8a85722e7bbf1361fdbb9656508a8eb14966624e
Thomas Ingram <code@ravinggenius.com>
2018-03-06 02:56:29 -0500

    General code cleanup

--- source/db/index.js
+++ source/db/index.js
@@ -3,15 +3,13 @@ const pgp = require('pg-promise');
 
 const config = require('../config');
 
-const { QueryFile } = pgp;
-
 module.exports = {
 	db: pgp()(config.databaseUrl),
 
 	loadQuery: (file) => {
 		const fullPath = path.join(__dirname, '..', file);
 
-		return new QueryFile(fullPath, {
+		return new pgp.QueryFile(fullPath, {
 			debug: true,
 			minify: true
 		});
