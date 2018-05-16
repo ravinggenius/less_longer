92018a09f072ad0e2cf76e8445c0aea1bfd82052
Thomas Ingram <code@ravinggenius.com>
2018-05-16 11:14:29 -0400

    Show fancy background colors
    Adapted from https://meowni.ca/

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,29 @@ import React from 'react';
 import { injectGlobal } from 'react-emotion';
 
 injectGlobal`
+	* {
+		box-sizing: border-box;
+	}
+
 	body {
+		background-attachment: fixed;
+		background-image:
+			linear-gradient(
+				rgba(255, 255, 255, 0.1) 0%,
+				rgba(0, 0, 0, 0.9) 100%
+			),
+			linear-gradient(
+				90deg,
+				#F79533 0%,
+				#F37055 15%,
+				#EF4E7B 30%,
+				#A166AB 44%,
+				#5073B8 58%,
+				#1098AD 72%,
+				#07B39B 86%,
+				#6DBA82 100%
+			);
+		font-family: sans-serif;
 		margin: 0;
 	}
 `;
