dfb58aa4ac67c56851481bbd65bb091c4233ed73
Thomas Ingram <code@ravinggenius.com>
2018-08-10 17:56:12 -0400

    Prefer functional stateless components

--- pages/s/index.jsx
+++ pages/s/index.jsx
@@ -7,65 +7,63 @@ import SlugForm from '../../components/slug_form';
 import SlugsList from '../../components/slug_list';
 import { H1, P } from '../../components/text';
 
-export default class SlugsIndex extends React.Component {
-	static defaultProps = {
-		baseUrl: '',
-		loading: true,
-		slugs: []
+const SlugsIndex = ({ baseUrl, loading, slugs }) => {
+	if (loading) {
+		return <P>Loading slugs....</P>;
 	}
 
-	static propTypes = {
-		baseUrl: PropTypes.string.isRequired,
-		loading: PropTypes.bool,
-		slugs: PropTypes.arrayOf(PropTypes.shape({
-			code: PropTypes.string.isRequired,
-			id: PropTypes.string.isRequired,
-			url: PropTypes.string.isRequired
-		})).isRequired
+	return <Layout header={<H1>Shortened URLs</H1>} title="Slugs">
+		<SlugForm />
+
+		<SlugsList {...{ baseUrl, slugs }} />
+	</Layout>;
+};
+
+SlugsIndex.defaultProps = {
+	baseUrl: '',
+	loading: true,
+	slugs: []
+};
+
+SlugsIndex.getInitialProps = async ({ req, query }) => {
+	if (req) {
+		return {
+			baseUrl: query.baseUrl,
+			loading: false,
+			slugs: query.slug ? [ query.slug ] : query.slugs
+		};
 	}
 
-	static async getInitialProps({ req, query }) {
-		if (req) {
-			return {
-				baseUrl: query.baseUrl,
-				loading: false,
-				slugs: query.slug ? [ query.slug ] : query.slugs
-			};
-		}
+	if (query.slugCode) {
+		const { baseUrl, slug } = await fetchAuthenticated(
+			'GET',
+			`/s/${query.slugCode}`
+		);
 
-		if (query.slugCode) {
-			const { baseUrl, slug } = await fetchAuthenticated(
-				'GET',
-				`/s/${query.slugCode}`
-			);
+		return {
+			baseUrl,
+			loading: false,
+			slugs: [ slug ]
+		};
+	} else {
+		const { baseUrl, slugs } = await fetchAuthenticated('GET', '/s');
 
-			return {
-				baseUrl,
-				loading: false,
-				slugs: [ slug ]
-			};
-		} else {
-			const { baseUrl, slugs } = await fetchAuthenticated('GET', '/s');
-
-			return {
-				baseUrl,
-				loading: false,
-				slugs
-			};
-		}
+		return {
+			baseUrl,
+			loading: false,
+			slugs
+		};
 	}
+};
 
-	render() {
-		const { baseUrl, loading, slugs } = this.props;
+SlugsIndex.propTypes = {
+	baseUrl: PropTypes.string.isRequired,
+	loading: PropTypes.bool,
+	slugs: PropTypes.arrayOf(PropTypes.shape({
+		code: PropTypes.string.isRequired,
+		id: PropTypes.string.isRequired,
+		url: PropTypes.string.isRequired
+	})).isRequired
+};
 
-		if (loading) {
-			return <P>Loading slugs....</P>;
-		}
-
-		return <Layout header={<H1>Shortened URLs</H1>} title="Slugs">
-			<SlugForm />
-
-			<SlugsList {...{ baseUrl, slugs }} />
-		</Layout>;
-	}
-}
+export default SlugsIndex;
