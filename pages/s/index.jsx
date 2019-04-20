037c25b38cf829ed2aef59d1ac339f318f81b9a4
Thomas Ingram <code@ravinggenius.com>
2019-04-20 13:11:14 -0400

    Keep state management in page components

--- pages/s/index.jsx
+++ pages/s/index.jsx
@@ -1,19 +1,131 @@
+import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import { withRouter } from 'next/router';
 import React from 'react';
 
-import { fetchAuthenticated } from '../../client/browser';
+import { fetchAuthenticated, fetchAuthenticatedBody } from '../../client/browser';
+import Button from '../../components/form/Button';
+import Form from '../../components/form/form';
+import Input from '../../components/form/input';
+import Toggle from '../../components/form/toggle';
 import Layout from '../../components/layouts/LinearLayout';
-import SlugForm from '../../components/slug_form';
 import SlugsList from '../../components/slug_list';
 import { H1, P } from '../../components/text';
 
-const SlugsIndex = ({ baseUrl, loading, slugs }) => {
+const URLInput = styled(Input)`
+	> input {
+		font-size: 40px;
+	}
+`;
+
+class SlugForm extends React.Component {
+	static propTypes = {
+		router: PropTypes.shape({}).isRequired
+	}
+
+	state = {
+		customize: false,
+		displayCode: '',
+		displayURL: ''
+	}
+
+	handleChange = (fieldname) => ({ target: { value } }) => {
+		this.setState(() => ({
+			[fieldname]: value
+		}));
+	}
+
+	handleToggle = (fieldname) => () => {
+		this.setState((previous) => ({
+			[fieldname]: !previous[fieldname]
+		}));
+	}
+
+	handleSubmit = async (event) => {
+		const { router } = this.props;
+		const {
+			customize,
+			displayCode,
+			displayURL
+		} = this.state;
+
+		event.preventDefault();
+
+		try {
+			const { data } = await fetchAuthenticatedBody('POST', '/s', {
+				code: customize ? displayCode : '',
+				url: displayURL
+			});
+
+			this.setState(() => ({
+				displayCode: '',
+				displayURL: '',
+				error: null
+			}));
+
+			router.replace(`/s/${data.code}`);
+		} catch ({ error }) {
+			this.setState(() => ({
+				error
+			}));
+		}
+	}
+
+	render() {
+		const {
+			customize,
+			displayCode,
+			displayURL,
+			error
+		} = this.state;
+
+		return <Form
+			{...{ error }}
+			action="/s"
+			method="post"
+			onSubmit={this.handleSubmit}
+		>
+			<URLInput
+				label="URL"
+				name="url"
+				onChange={this.handleChange('displayURL')}
+				type="url"
+				value={displayURL}
+			/>
+
+			<Input
+				disabled={!customize}
+				label="Code"
+				name="code"
+				onChange={this.handleChange('displayCode')}
+				value={displayCode}
+			/>
+
+			<div css={`
+				align-items: center;
+				display: flex;
+			`}>
+				<Toggle
+					label="Customize Code?"
+					multiSelect
+					name="customize"
+					onChange={this.handleToggle('customize')}
+					value={customize}
+				/>
+
+				<Button type="submit">Lessify!</Button>
+			</div>
+		</Form >;
+	}
+}
+
+const SlugsIndex = ({ baseUrl, loading, router, slugs }) => {
 	if (loading) {
 		return <P>Loading slugs....</P>;
 	}
 
 	return <Layout header={<H1>Shortened URLs</H1>} title="Slugs">
-		<SlugForm />
+		<SlugForm {...{ router }} />
 
 		<SlugsList {...{ baseUrl, slugs }} />
 	</Layout>;
@@ -59,6 +171,7 @@ SlugsIndex.getInitialProps = async ({ req, query }) => {
 SlugsIndex.propTypes = {
 	baseUrl: PropTypes.string.isRequired,
 	loading: PropTypes.bool,
+	router: PropTypes.shape({}).isRequired,
 	slugs: PropTypes.arrayOf(PropTypes.shape({
 		code: PropTypes.string.isRequired,
 		id: PropTypes.string.isRequired,
@@ -66,4 +179,4 @@ SlugsIndex.propTypes = {
 	})).isRequired
 };
 
-export default SlugsIndex;
+export default withRouter(SlugsIndex);
